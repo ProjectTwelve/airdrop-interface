3a498896c0c955cfed2243612fe76d5d185ce3b5
linchengzzz <1139489076@qq.com>
2022-05-31 21:46:25 +0800

    perf: update GL effect

--- components/butterflyGL/index.tsx
+++ components/butterflyGL/index.tsx
@@ -1,7 +1,14 @@
-import { useEffect, useRef, useState } from 'react';
 import Script from 'next/script';
-import butterflyHelpers from './butterflyHelpers';
 import { useEvent } from 'react-use';
+import { useEffect, useRef, useState } from 'react';
+import butterflyHelpers from './butterflyHelpers';
+
+const themes = {
+  theme1: [0xff9e76, 0xa011ff],
+  theme2: [0xa3d365, 0x09bda0],
+  theme3: [0xffb876, 0xd9214d],
+  theme4: [0x3592ff, 0xa072ff],
+};
 
 export default function ButterflyGL() {
   const [isLoaded, setIsLoaded] = useState(false);
@@ -39,13 +46,15 @@ export default function ButterflyGL() {
   useEffect(() => {
     if (isLoaded) {
       const butterfly = window.hpgButterfly;
-      const theme = butterflyHelpers.defaultProperties.theme1;
+      const defaultProperties = butterflyHelpers.defaultProperties;
+      defaultProperties.color1Hex = themes.theme1[0];
+      defaultProperties.color2Hex = themes.theme1[1];
       if (!butterfly.checkIsSupported(canvasRef.current)) {
         return;
       }
       butterfly.init();
       butterfly.properties.scene.children = butterfly.properties.scene.children.filter((v: any) => v.type !== 'Object3D');
-      Object.assign(butterfly.properties, theme);
+      Object.assign(butterfly.properties, defaultProperties);
       butterfly.resize(window.innerWidth, window.innerHeight);
       setVisibility(true);
       butterflyRef.current = butterfly;
@@ -66,6 +75,7 @@ export default function ButterflyGL() {
     }
     return () => {
       if (animationIdRef.current) {
+        butterfly && butterfly.reset();
         cancelAnimationFrame(animationIdRef.current);
         animationIdRef.current = undefined;
       }
@@ -75,7 +85,7 @@ export default function ButterflyGL() {
   return (
     <>
       <Script id="butterfly" src="/js/butterfly.min.js" strategy="lazyOnload" onLoad={() => setIsLoaded(true)} />
-      <div ref={containerRef} className="butterfly-gl fixed top-0 left-0 right-0 bottom-0 -z-10 opacity-40">
+      <div ref={containerRef} className="butterfly-gl fixed top-0 left-0 right-0 bottom-0 -z-10 opacity-75">
         <canvas ref={canvasRef} className="butterfly-canvas absolute h-full w-full"></canvas>
       </div>
     </>
