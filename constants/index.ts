9cb9ad2bc3529aaeefd2562c195a16add9cead74
linchengzzz <1139489076@qq.com>
2022-06-07 20:09:51 +0800

    refactor: update assets

--- constants/index.ts
+++ constants/index.ts
@@ -1,24 +1,24 @@
 import { NFTLevel } from '../store/developer/state';
 
 export const CHAIN_ID = parseInt(process.env.NEXT_PUBLIC_CHAIN_ID ?? '1');
-export const NFT_CONTRACT_ADDRESS = '0x900b8215FA5231C24f30C694850481e25300845c';
+export const NFT_CONTRACT_ADDRESS = '0xb034d6bA0b6593Fa5107C6a55042b67746d44605';
 export const GALAXY_LIST = 'https://galaxy.eco/mynfts/list';
 
 export const BADGES = {
   [NFTLevel.ORANGE]: {
-    asset: 'https://cdn.galaxy.eco/galaxy/assets/projecttwelve/1653648412040155648.gif',
-    claim: 'https://galaxy.eco/P12/campaign/GCMXMUUTBT',
+    asset: 'https://cdn.galaxy.eco/galaxy/assets/projecttwelve/1653656759076802901.gif',
+    claim: 'https://galaxy.eco/P12/campaign/GCt9JUUJCJ',
   },
   [NFTLevel.PURPLE]: {
-    asset: 'https://cdn.galaxy.eco/galaxy/assets/projecttwelve/1653648305835037699.gif',
-    claim: 'https://galaxy.eco/P12/campaign/GCnKMUUwa5',
+    asset: 'https://cdn.galaxy.eco/galaxy/assets/projecttwelve/1653654265642526889.gif',
+    claim: 'https://galaxy.eco/P12/campaign/GCteJUUM7z',
   },
   [NFTLevel.BLUE]: {
-    asset: 'https://cdn.galaxy.eco/galaxy/assets/projecttwelve/1653648610530074458.gif',
-    claim: 'https://galaxy.eco/P12/campaign/GCnJMUUwZb',
+    asset: 'https://cdn.galaxy.eco/galaxy/assets/projecttwelve/1653653761673395155.gif',
+    claim: 'https://galaxy.eco/P12/campaign/GCi9CUUrni',
   },
   [NFTLevel.GREEN]: {
-    asset: 'https://cdn.galaxy.eco/galaxy/assets/projecttwelve/1653648206887517339.gif',
-    claim: 'https://galaxy.eco/P12/campaign/GCJyMUUdVA',
+    asset: 'https://cdn.galaxy.eco/galaxy/assets/projecttwelve/1653653545770629768.gif',
+    claim: 'https://galaxy.eco/P12/campaign/GCV4JUU7eR',
   },
 };
