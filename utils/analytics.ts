bb73d224117dbf2d19d73c19183445b2515cefae
linchengzzz <1139489076@qq.com>
2022-07-01 00:25:18 +0800

    chore: upgrade ga config

--- utils/analytics.ts
+++ utils/analytics.ts
@@ -1,12 +1,9 @@
 import ReactGA from 'react-ga4';
-import { isBrowser } from './index';
-import { getLocalStorage } from './storage';
 
 const GOOGLE_ANALYTICS_ID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID;
-const debug_mode = isBrowser ? getLocalStorage('ga_debug') : false;
 
 if (GOOGLE_ANALYTICS_ID) {
-  ReactGA.initialize(GOOGLE_ANALYTICS_ID, { gtagOptions: { debug_mode } });
+  ReactGA.initialize(GOOGLE_ANALYTICS_ID);
 } else {
-  ReactGA.initialize('test', { testMode: true, gtagOptions: { debug_mode } });
+  ReactGA.initialize('test', { testMode: true });
 }
