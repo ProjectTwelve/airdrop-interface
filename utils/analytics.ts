ce44ed688b80d71ad6c5fa61e6744196e7a3534a
linchengzzz <1139489076@qq.com>
2022-06-30 15:15:36 +0800

    feat: add ga debug

--- utils/analytics.ts
+++ utils/analytics.ts
@@ -1,10 +1,12 @@
 import ReactGA from 'react-ga4';
+import { isBrowser } from './index';
+import { getLocalStorage } from './storage';
 
 const GOOGLE_ANALYTICS_ID: string | undefined = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID;
+const debug_mode = isBrowser ? getLocalStorage('ga_debug') : false;
 
 if (typeof GOOGLE_ANALYTICS_ID === 'string') {
-  ReactGA.initialize(GOOGLE_ANALYTICS_ID, { gaOptions: { storage: 'none', storeGac: false } });
-  ReactGA.set({ anonymizeIp: true });
+  ReactGA.initialize(GOOGLE_ANALYTICS_ID, { gtagOptions: { debug_mode } });
 } else {
-  ReactGA.initialize('test', { gtagOptions: { debug_mode: true } });
+  ReactGA.initialize('test', { gtagOptions: { debug_mode } });
 }
