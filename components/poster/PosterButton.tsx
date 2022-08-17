44733adff897e923b0acf745dc4faa59ecf40350
linchengzzz <1139489076@qq.com>
2022-08-17 20:00:07 +0800

    chore: poster text

--- components/poster/PosterButton.tsx
+++ components/poster/PosterButton.tsx
@@ -1,14 +1,16 @@
 import React, { useEffect } from 'react';
+import { useAccount } from 'wagmi';
+import { useRouter } from 'next/router';
+import { useRecoilState, useRecoilValue } from 'recoil';
 import { motion, AnimatePresence } from 'framer-motion';
 import { getLocalStorage } from '../../utils/storage';
-import { useAccount } from 'wagmi';
-import { useRecoilState, useRecoilValue } from 'recoil';
 import { gamerClaimedPosterAtom } from '../../store/gamer/state';
 import { posterBtnShowAtom, posterCaptureAtom } from '../../store/poster/state';
 import { STORAGE_KEY } from '../../constants';
 
 export default function PosterButton() {
   const { address } = useAccount();
+  const router = useRouter();
   const [gamerClaimedPoster, setGamerClaimedPoster] = useRecoilState(gamerClaimedPosterAtom);
   const [show, setShow] = useRecoilState<boolean>(posterBtnShowAtom);
   const posterCapture = useRecoilValue(posterCaptureAtom);
@@ -31,7 +33,8 @@ export default function PosterButton() {
     <div className="relative">
       <div onClick={onClick} className="poster__button mr-3 h-[40px] cursor-pointer">
         <div className="flex h-full w-full items-center justify-center bg-p12-gradient-30 px-[18px] text-sm hover:bg-p12-gradient-45">
-          <img src="/svg/picture.svg" width="24" height="24" className="mr-1 xs:hidden" alt="picture" /> My Poster
+          <img src="/svg/picture.svg" width="24" height="24" className="mr-1 xs:hidden" alt="picture" />
+          {router.pathname === '/gamer/[address]' ? 'Poster' : 'My Poster'}
         </div>
       </div>
       <AnimatePresence>
