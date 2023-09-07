489bcbccf78018901369db25317fd4e3c638fc8f
linchengzzz <1139489076@qq.com>
2023-09-07 16:48:17 +0800

    feat: add landing site

--- components/layout/LayoutFooter.tsx
+++ components/layout/LayoutFooter.tsx
@@ -1,21 +1,7 @@
 import React from 'react';
-import ReactGA from 'react-ga4';
 
 function LayoutFooter() {
-  return (
-    <div className="flex items-center justify-center py-4">
-      <a
-        className="text-sm"
-        href="https://p12.network/"
-        target="_blank"
-        onClick={() => {
-          ReactGA.event({ action: 'to_site', label: '', category: 'bridge' });
-        }}
-      >
-        To P12 Website -&gt;
-      </a>
-    </div>
-  );
+  return <div className="flex items-center justify-center py-4" />;
 }
 
 export default React.memo(LayoutFooter);
--- components/layout/LayoutHeaderExtra.tsx
+++ components/layout/LayoutHeaderExtra.tsx
@@ -15,6 +15,7 @@ function LayoutHeaderExtra() {
   const setInviteOpen = useSetRecoilState(inviteModalAtom);
   const invitationCount = useRecoilValue(invitationCountSelector);
   const readmeLink = 'https://github.com/ProjectTwelve/airdrop-interface#readme';
+  const landingSite = 'https://p12.network';
   const hideRoute = ['/', '/arcana/[[...address]]', '/collab/qatar2022', '/bridge'];
 
   useEffect(() => {
@@ -23,69 +24,77 @@ function LayoutHeaderExtra() {
   }, []);
 
   return (
-    <AnimatePresence>
-      {!hideRoute.includes(router.pathname) && (
-        <motion.div className="relative">
-          <motion.div
-            initial={{ opacity: 0.45 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0, transition: { duration: 0.2 } }}
-            transition={{ type: 'spring', stiffness: 200, damping: 30 }}
-            className="relative flex"
-          >
-            <Button type="bordered" className="mr-3 lg:hidden xl:hidden" onClick={() => openLink(readmeLink)}>
-              <div className="flex items-center justify-center text-sm">
-                <img src="/svg/white_paper.svg" width={24} height={24} alt="white_paper" />
-                &nbsp;Readme
-              </div>
-            </Button>
-            {router.pathname !== '/gamer/[address]' && (
-              <Button
-                type="bordered"
-                onClick={() => {
-                  ReactGA.event({ action: 'Invite', category: 'Click', label: 'Header' });
-                  setInviteOpen(true);
-                }}
-              >
+    <div className="flex">
+      <Button type="bordered" className="mr-3 lg:hidden" onClick={() => openLink(landingSite)}>
+        <div className="flex items-center justify-center text-sm">
+          <img src="/svg/landing.svg" width={24} height={24} alt="landing_site" />
+          &nbsp;P12 Landingsite
+        </div>
+      </Button>
+      <AnimatePresence>
+        {!hideRoute.includes(router.pathname) && (
+          <motion.div className="relative">
+            <motion.div
+              initial={{ opacity: 0.45 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0, transition: { duration: 0.2 } }}
+              transition={{ type: 'spring', stiffness: 200, damping: 30 }}
+              className="relative flex"
+            >
+              <Button type="bordered" className="mr-3 lg:hidden xl:hidden" onClick={() => openLink(readmeLink)}>
                 <div className="flex items-center justify-center text-sm">
-                  <img src="/svg/invite.svg" width={24} height={24} alt="invite" />
-                  &nbsp;My referral link
-                  <p className="ml-3 border-l-2 border-gray-600 pl-3 font-ddin text-xl font-bold text-green lg:hidden">
-                    {invitationCount}
-                  </p>
+                  <img src="/svg/white_paper.svg" width={24} height={24} alt="white_paper" />
+                  &nbsp;Readme
                 </div>
               </Button>
-            )}
-            <AnimatePresence>
-              {!tipsClick && (
-                <motion.div
-                  initial={{ right: 0, opacity: 0, scale: 0.85 }}
-                  animate={{ right: -200, opacity: 1, scale: 1 }}
-                  exit={{ right: 0, opacity: 0, scale: 0.85 }}
-                  transition={{ type: 'spring', damping: 20, stiffness: 300 }}
-                  className="tooltip__container absolute -top-1/2 z-10 w-[190px] rounded-lg text-xs"
+              {router.pathname !== '/gamer/[address]' && (
+                <Button
+                  type="bordered"
+                  onClick={() => {
+                    ReactGA.event({ action: 'Invite', category: 'Click', label: 'Header' });
+                    setInviteOpen(true);
+                  }}
                 >
-                  <div className="p-4">
-                    Share invite address get rewards, It&apos;s dangerous to go alone, take this
-                    <div className="flex justify-end">
-                      <div
-                        className="cursor-pointer text-blue"
-                        onClick={() => {
-                          setTipsClick(true);
-                          setLocalStorage(STORAGE_KEY.INVITE_TIPS_CLICK, true);
-                        }}
-                      >
-                        Okay
+                  <div className="flex items-center justify-center text-sm">
+                    <img src="/svg/invite.svg" width={24} height={24} alt="invite" />
+                    &nbsp;My referral link
+                    <p className="ml-3 border-l-2 border-gray-600 pl-3 font-ddin text-xl font-bold text-green lg:hidden">
+                      {invitationCount}
+                    </p>
+                  </div>
+                </Button>
+              )}
+              <AnimatePresence>
+                {!tipsClick && (
+                  <motion.div
+                    initial={{ right: 0, opacity: 0, scale: 0.85 }}
+                    animate={{ right: -200, opacity: 1, scale: 1 }}
+                    exit={{ right: 0, opacity: 0, scale: 0.85 }}
+                    transition={{ type: 'spring', damping: 20, stiffness: 300 }}
+                    className="tooltip__container absolute -top-1/2 z-10 w-[190px] rounded-lg text-xs"
+                  >
+                    <div className="p-4">
+                      Share invite address get rewards, It&apos;s dangerous to go alone, take this
+                      <div className="flex justify-end">
+                        <div
+                          className="cursor-pointer text-blue"
+                          onClick={() => {
+                            setTipsClick(true);
+                            setLocalStorage(STORAGE_KEY.INVITE_TIPS_CLICK, true);
+                          }}
+                        >
+                          Okay
+                        </div>
                       </div>
                     </div>
-                  </div>
-                </motion.div>
-              )}
-            </AnimatePresence>
+                  </motion.div>
+                )}
+              </AnimatePresence>
+            </motion.div>
           </motion.div>
-        </motion.div>
-      )}
-    </AnimatePresence>
+        )}
+      </AnimatePresence>
+    </div>
   );
 }
 
