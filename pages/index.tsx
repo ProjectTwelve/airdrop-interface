270c37619366bc21e7cb0ad5f6ff2fec65f16b83
linchengzzz <1139489076@qq.com>
2023-09-14 19:25:08 +0800

    feat: change banner

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,19 +6,22 @@ import CollabSwiper from '@/components/collab/CollabSwiper';
 import PowerLevelBanner from '@/components/pl/PowerLevelBanner';
 
 export default function Home() {
-  const src = useThemeAsset('arcana-banner.webp');
+  const src = useThemeAsset('arcana_banner_2.webp');
 
   return (
     <div className="flex flex-col items-center justify-center pt-4">
-      <div className="mt-4 grid w-full grid-cols-2 gap-4 md:grid-cols-1">
+      {/*<div className="mt-4 grid w-full grid-cols-2 gap-4 md:grid-cols-1">*/}
+      <div className="mt-4  flex w-full gap-4 md:flex-col">
         <div
-          className="cursor-pointer overflow-hidden rounded-2xl duration-200 ease-linear hover:-translate-y-1"
+          className="cursor-pointer rounded-2xl duration-200 ease-linear hover:-translate-y-1"
           onClick={() => openLink('https://arcana.p12.games/referral?code=Mkq4zW')}
         >
           {src ? (
-            <img className="h-[300px] object-cover object-center md:h-[128px]" src={src} alt="p12Arcana" />
+            <img className="-mt-[20px] h-[320px] w-[684px] object-cover rounded-2xl object-center md:h-auto md:w-full" src={src} alt="p12Arcana" />
           ) : (
-            <div className="h-[300px] w-full animate-pulse bg-white/10" />
+            // <img className="h-[300px] object-cover object-center md:h-[128px]" src={src} alt="p12Arcana" />
+            // <div className="h-[300px] w-1/2 animate-pulse bg-white/10" />
+            <div className="h-[300px] w-[684px] animate-pulse bg-white/10" />
           )}
         </div>
         <CollabSwiper />
